59827a659ae89c16678d0b01f70c18c703769079
Lin <Lin@DESKTOP-EOF2L9K>
2023-02-13 20:48:59 +0200

    30.01

--- app/src/main/java/com/example/basics/MainActivity.java
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = findViewById( R.id.addButton ) ;
+        addButton.setOnClickListener( this::btnAddExclamationClick ) ;
+    }
+
+    private void btnAddExclamationClick( View v ) {
+        TextView tvHello = findViewById( R.id.tvHello ) ;
+        String txt = tvHello.getText().toString() ;
+        txt += "!" ;
+        tvHello.setText( txt ) ;
     }
 }
