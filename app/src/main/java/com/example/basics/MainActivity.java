486021071ff1ea16f855dfbd5cda456a024324d2
Lin <Lin@DESKTOP-EOF2L9K>
2023-02-21 03:05:01 +0200

    31.01

--- app/src/main/java/com/example/basics/MainActivity.java
+++ app/src/main/java/com/example/basics/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.basics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addButton = findViewById( R.id.addButton ) ;
-        addButton.setOnClickListener( this::btnAddExclamationClick ) ;
+        Button calcButton = findViewById( R.id.calcButton ) ;
+        calcButton.setOnClickListener( this::btnCalcClick ) ;
+
+        findViewById( R.id.exitButton )
+                .setOnClickListener( this::btnExitClick ) ;
     }
 
-    private void btnAddExclamationClick( View v ) {
-        TextView tvHello = findViewById( R.id.tvHello ) ;
-        String txt = tvHello.getText().toString() ;
-        txt += "!" ;
-        tvHello.setText( txt ) ;
+    private void btnCalcClick( View v ) {
+        Intent calcIntent = new Intent(
+                MainActivity.this,
+                CalcActivity.class ) ;
+        startActivity( calcIntent ) ;
+    }
+    private void btnExitClick( View v ) {
+        finish() ;
     }
 }
